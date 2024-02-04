23d64c2c983a86b0aa66c278257f691d6c4c58cb
abdurrahman086 <rahmanabdur15689@gmail.com>
2024-02-05 02:31:36 +0700

    ui login

---
+++ android/app/src/main/kotlin/com/example/login_regis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.login_regis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = Colors.white;
+Color textColor = const Color.fromARGB(255, 0, 29, 53);
+Color secondaryTextColor = Color.fromARGB(255, 3, 57, 102);
+Color buttonColor = Color.fromARGB(255, 3, 57, 102);
+Color secondaryButtonColor = Color.fromARGB(255, 0, 101, 184);
+Color rememberMeColor = Colors.grey;
+
+TextStyle whiteTextStyle = GoogleFonts.dmSans(
+  color: whiteColor,
+);
+TextStyle blackTextStyle = GoogleFonts.dmSans(
+  color: textColor,
+);
+TextStyle secondaryTextStyle = GoogleFonts.dmSans(
+  color: secondaryTextColor,
+);
+TextStyle buttonTextStyle = GoogleFonts.dmSans(
+  color: buttonColor,
+);
+TextStyle rememberTextStyle = GoogleFonts.dmSans(
+  color: rememberMeColor,
+);
